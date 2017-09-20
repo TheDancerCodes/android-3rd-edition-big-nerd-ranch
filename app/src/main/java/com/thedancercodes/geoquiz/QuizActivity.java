5246d2b0a26db2fa395c7ef6b98e5f4991b26ce9
Roger Taracha <rojtaracha@gmail.com>
2017-09-20 12:31:19 +0300

    [FEATURE] Customizing the Toast: [+] Customize the toast to show at the top instead of the bottom of the screen.

--- app/src/main/java/com/thedancercodes/geoquiz/QuizActivity.java
+++ app/src/main/java/com/thedancercodes/geoquiz/QuizActivity.java
@@ -2,9 +2,25 @@ package com.thedancercodes.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+
+/**
+ * CHALLENGE: Customizing the Toast
+ *
+ * 1) You are to customize the toast to show at the top instead of the bottom of the screen.
+ *
+ * 2) To change how the toast is displayed, use the Toast class's setGravity method.
+ *
+ * 3) Use Gravity.TOP for the gravity value.
+ *
+ * 4) Refer to the developer documentation at
+ * https://developer.android.com/reference/android/widget/Toast.html#setGravity(int, int, int)
+ * for more details.
+ */
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -21,16 +37,23 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(QuizActivity.this,
+                               R.string.correct_toast,
+                               Toast.LENGTH_SHORT);
+
+                toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 200);
+                toast.show();
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this,
+                Toast toast = Toast.makeText(QuizActivity.this,
                                R.string.incorrect_toast,
-                               Toast.LENGTH_SHORT).show();
+                               Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 200);
+                toast.show();
             }
         });
     }
